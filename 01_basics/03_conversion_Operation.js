dfc97b150e529fc3bfbafcae9da2b7f4aed4e855
NageshPanchal007 <157118002+NageshPanchal007@users.noreply.github.com>
2025-05-17 09:54:57 +0000

    conversion

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,21 @@
+let score="23abc"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+
+
+// let isLoggedIn=0
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+let someNumber=32
+
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log(typeof(StringNumber));
+
+
